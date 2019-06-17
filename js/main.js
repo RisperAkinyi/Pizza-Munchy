ebf941757471c20b7384adae20f5f7e42443539e
Risper Akinyi <email@example.com>
2019-06-17 15:40:34 +0300

    Add calcution so that the customer is able to place an order and get the full charge of the pizza ordered

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var totalCosts = [];
 function order(size, crust, meatyTopping, veggyTopping ){
     this.size = size;
     this.crust = crust;
@@ -8,50 +9,81 @@ function order(size, crust, meatyTopping, veggyTopping ){
 }
 var pizzaSize = ["Small", "Medium", "Large"];
 var pizzaCrust = ["Crispy", "Stuffed", "Gluten-free"];
-var pizzaMeatyTopping = ["Pepperoni", "Sausage", "Chicken",];
-var pizzaVeggyTopping = ["Onion", "Pineapple", "Spinach",];
+var pizzaMeatyTopping = ["Peperonni", "Sausage", "Chicken",];
+var pizzaveggyTopping = ["Onion", "Pineapple", "Spinach",];
 
 order.prototype.cost = function(){
     if (this.size === pizzaSize[0]){
-        this.prize += 300;
+        this.price += 300;
     }
     else if (this.size === pizzaSize[1]){
-        this.prize += 500;
+        this.price += 500;
     }
     else if (this.size === pizzaSize[2]){
-        this.prize += 600;
+        this.price += 600;
     }
 
     if (this.crust === pizzaCrust[0]){
-        this.prize += 100;
+        this.price += 100;
     }
-    else if (this.size === pizzaCrust[1]){
-        this.prize += 130;
+    else if (this.crust === pizzaCrust[1]){
+        this.price += 130;
     }
-    else if (this.size === pizzaCrust[2]){
-        this.prize += 150;
+    else if (this.crust === pizzaCrust[2]){
+        this.price += 150;
     }
 
     if (this.meatyTopping === pizzaMeatyTopping[0]){
-        this.prize += 100;
+        this.price += 100;
     }
-    else if (this.size === pizzaMeatyTopping[1]){
-        this.prize += 100;
+    else if (this.meatyTopping === pizzaMeatyTopping[1]){
+        this.price += 100;
     }
-    else if (this.size === pizzaMeatyTopping[2]){
-        this.prize += 100;
+    else if (this.meatyTopping === pizzaMeatyTopping[2]){
+        this.price += 100;
     }
 
 
     if (this.veggyTopping === pizzaveggyTopping[0]){
-        this.prize += 100;
+        this.price += 100;
     }
-    else if (this.size === pizzaveggyTopping[1]){
-        this.prize += 100;
+    else if (this.veggyTopping === pizzaveggyTopping[1]){
+        this.price += 100;
     }
-    else if (this.size === pizzaveggyTopping[2]){
-        this.prize += 100;
+    else if (this.veggyTopping === pizzaveggyTopping[2]){
+        this.price += 100;
     }
 
+    return this.price;
 
 }
+order.prototype.totalCost = function(){
+    var orderTotal = 0;
+    for(var order =0; order < totalCosts.length; order++){
+        orderTotal += totalCosts[order];
+    }
+    return orderTotal;
+}
+
+$(document).ready(function () {
+    $("input#totalCost").click(function (event) { 
+        event.preventDefault();
+        var sizes = $("select#size").val();
+        var crusts = $("select#crust").val();
+        var meatyToppings = $("select#meaty").val();
+        var veggyToppings = $("select#veggy").val();
+
+        var newPizzaOrder = new order(sizes, crusts, meatyToppings, veggyToppings);
+        newPizzaOrder.cost();
+        totalCosts.push(newPizzaOrder.price);
+
+        $("#pz").text(sizes);
+        $("#cr").text(crusts);
+        $("#mt").text(meatyToppings);
+        $("#vt").text(veggyToppings);
+        $("#tc").text(newPizzaOrder.totalCost());
+
+        
+    }); 
+});
+
